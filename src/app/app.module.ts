702a35934279820abf19ceebad096a3d8853f28e
Akshat202002 <akshatsamdani23@gmail.com>
2023-06-19 16:42:28 +0530

    app-routing demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { Task3Component } from './task3/task3.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { Task4Module } from './task4/task4.module';
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    Task3Component,
     PageNotFoundComponent
   ],
   imports: [
